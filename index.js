87909aa7805cd07f3998e8a5690a663fbc6e0ab5
Rosie Allison-Corio <rosievespa@gmail.com>
2025-01-21 12:00:30 +0000

    chalk NPM tested

---
+++ index.js
@@ -0,0 +1,8 @@
+const log = console.log
+import chalk from "chalk"
+
+console.log(chalk.blue("Hello world!"));
+
+console.log(chalk.bgGray("Hello there Rosie!"))
+
+log(chalk.blue.bgRed.bold('Hello world!'));
